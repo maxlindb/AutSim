84427e93a496b12be62839b952f323165037b795
maxlindb <max.lindblad@gmail.com>
2024-11-21 20:26:12 +0200

    work till 18:15 of logging video, till the unicode compilation error

---
+++ AutSim/src/AutSim/Log.cpp
@@ -0,0 +1,14 @@
+#include "Log.h"
+
+namespace AutSim {
+
+	std::shared_ptr<spdlog::logger> Log::s_CoreLogger;
+	std::shared_ptr<spdlog::logger> Log::s_ClientLogger;
+
+
+	void Log::Init() 
+	{
+
+	}
+
+}
